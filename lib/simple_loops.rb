b5df68af61bdbc1284e371a2a7422078b8efcc67
Daniel Hahm <dhahm01@gmail.com>
2020-06-07 20:22:28 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,7 +1,7 @@
 # Write your methods here
 def loop_message_five_times(message)
   counter = 0 
-  while counter < message.length do 
+  while counter < 5 do 
     puts message
     counter++
   end
